addecb28ea9bc83175d5a9abdef48b07f8762f0a
SummerLKun <474300282@qq.com>
2016-08-31 11:04:43 +0800

    add GoodsBean

---
+++ src/com/soft/bean/GoodsBean.java
@@ -0,0 +1,47 @@
+package com.soft.bean;
+
+public class GoodsBean {
+	public String name;
+	public String img;
+	public String detail;
+	public String price;
+	public String type;
+	public String tel;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+}
